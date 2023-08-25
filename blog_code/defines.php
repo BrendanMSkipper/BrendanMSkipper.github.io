e7e7b8d7dd75604bb477cddf9eaf91db698364dd
br3ndan56 <143013646+br3ndan56@users.noreply.github.com>
2023-08-25 23:49:04 +0700

    Add files via upload

---
+++ blog_code/defines.php
@@ -0,0 +1,6 @@
+<?php
+    session_start();
+
+    define('FACEBOOK_APP_ID', '2651016275051359');
+    define('FACEBOOK_APP_SECRET', '●●●●●●●●');
+    define('FACEBOOK_REDIRECT_URI', 'https://br3ndan56.github.io/blog_code/obtaining_access_token.php/');
