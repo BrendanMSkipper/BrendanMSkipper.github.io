4c40a54db29549ae6682b415ce7c5a7914526ff8
BrendanMSkipper <143013646+BrendanMSkipper@users.noreply.github.com>
2023-08-26 16:27:41 +0700

    Update defines.php

--- blog_code/defines.php
+++ blog_code/defines.php
@@ -3,4 +3,4 @@
 
     define('FACEBOOK_APP_ID', '2651016275051359');
     define('FACEBOOK_APP_SECRET', '●●●●●●●●');
-    define('FACEBOOK_REDIRECT_URI', 'https://br3ndan56.github.io/blog_code/obtaining_access_token.php/');
+    define('FACEBOOK_REDIRECT_URI', 'https://brendanmskipper.github.io/blog_code/obtaining_access_token.php/');
